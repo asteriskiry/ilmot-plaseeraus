94879de2944e08640c2ad378c0f29bf38d3b7b35
Roosa Virta <roosa.virta@gmail.com>
2019-04-14 18:34:28 +0300

    Initial commit

---
+++ Plaseeraus.py
@@ -0,0 +1,13 @@
+#Alkuun oliolista osallistujista
+
+lajittelemattomat = []
+# for looppi tallentamaan kaikki osallistujat
+# lajittelemattomat listaan
+
+i=1
+j=1
+n=len(lajittelemattomat)
+
+poyta =[[n][2]]
+# poyta.[x][y], jos x on parillinen nainen oikealla, parittomalla nainen vasemmalla
+# eli jos [0][0] niin nainen menee x ja mies y
