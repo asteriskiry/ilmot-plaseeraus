528c5815dc147916dfd7c297f6f943a55b442d32
ThistleFIN <roosa.virta@gmail.com>
2019-04-14 21:44:25 +0300

    Lisäsin muutaman asteen

--- Plaseeraus.py
+++ Plaseeraus.py
@@ -2,9 +2,10 @@
 
 lajittelemattomat = []
 # for looppi tallentamaan kaikki osallistujat
+# lajittelemattomat = [Participant.objects.filter(event_type=uid)]
 # lajittelemattomat listaan
 
-i=1
+i=0
 j=1
 n=len(lajittelemattomat)
 
@@ -12,3 +13,7 @@ poyta =[[n][2]]
 # poyta.[x][y], jos x on parillinen nainen oikealla, parittomalla nainen vasemmalla
 # eli jos [0][0] niin nainen menee x ja mies y
 
+if(lajittelemattomat[i].sukupuoli='mies' || lajittelemattomat[i].sukupuoli='muu')
+    asdasd
+if(lajittelemattomat[i].sukupuoli='nainen' || lajittelemattomat[i].sukupuoli='muu')
+    sdfsdf
