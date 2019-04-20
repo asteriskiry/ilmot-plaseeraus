c89bf5149f83572d712eeb3ebfbe2bdafd8830ec
ThistleFIN <roosa.virta@gmail.com>
2019-04-20 13:20:54 +0300

    Algoritmi about 25% valmis, excuse my java, korjattu javan operaattorit pythoniksi

--- Plaseeraus.py
+++ Plaseeraus.py
@@ -16,11 +16,11 @@ poyta =[[n][2]]
 # poyta.[x][y], jos x on parillinen nainen oikealla, parittomalla nainen vasemmalla
 # eli jos [0][0] niin nainen menee x ja mies y
 
-if(lajittelemattomat[kohta].sukupuoli !='mies' && lajittelemattomat[kohta] !=null): #On siis nainen tai muu
-    if(i%%2=0 && j%%2=0 && poyta[i][j]==null): #on siis parillinen ja parillinen
+if(lajittelemattomat[kohta].sukupuoli !='mies' and lajittelemattomat[kohta] !=null): #On siis nainen tai muu
+    if(i%2==0 and j%2==0 and poyta[i][j]==null): #on siis parillinen ja parillinen
         lajittelemattomat[kohta]=poyta[i][j]
         i+=1
-    elif(i%%2=0 && j%%2=1 && poyta[i][j]==null): #on siis parillinen ja pariton
+    elif(i%2==0 and j%2==1 and poyta[i][j]==null): #on siis parillinen ja pariton
         lajittelemattomat[kohta]=poyta[i][j]
         j+=1
     elif(poyta[i][j] != null):
