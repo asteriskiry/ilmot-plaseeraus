f4ea7ca9c13d1cf582e56daaddf38df8e3818ba1
ThistleFIN <roosa.virta@gmail.com>
2019-04-20 18:18:39 +0300

    Algoritmi about 50% valmis, muokkasin asiat funktioiksi

--- Plaseeraus.py
+++ Plaseeraus.py
@@ -8,22 +8,23 @@ shuffle(lajittelemattomat)
 kohta = 0
 i = 0
 j = 0
+k = 0
 n = len(lajittelemattomat)
 
 poyta = [[n][2]]
 # poyta.[x][y], jos x on parillinen nainen oikealla, parittomalla nainen vasemmalla
 # eli jos [0][0] niin nainen menee x ja mies y
 
-while kohta < n:
-    if lajittelemattomat[kohta].sukupuoli != 'mies' and lajittelemattomat[kohta] != null:
+def istumaan(henkilo):
+    if henkilo.sukupuoli != 'mies' and lajittelemattomat[kohta] != null:
         # On siis nainen tai muu
         if i % 2 == 0 and j % 2 == 0 and poyta[i][j] == null:
             # On siis parillinen ja parillinen
-            lajittelemattomat[kohta] = poyta[i][j]
+            henkilo = poyta[i][j]
             i += 1
         elif i % 2 == 0 and j % 2 == 1 and poyta[i][j] == null:
             # On siis parillinen ja pariton
-            lajittelemattomat[kohta] = poyta[i][j]
+            henkilo = poyta[i][j]
             j += 1
         elif poyta[i][j] != null:
             if j == 2:
@@ -31,22 +32,22 @@ while kohta < n:
             if i == n:
                 i = 0
             if poyta[i+1][j] == null:
-                poyta[i+1][j] = lajittelemattomat[kohta]
+                poyta[i+1][j] = henkilo
             elif poyta[i][j+1] == null:
-                poyta[i][j+1] = lajittelemattomat[kohta]
+                poyta[i][j+1] = henkilo
         kohta += 1
 
         if j == 2:
             j = 0
 
-    if lajittelemattomat[kohta].sukupuoli != 'nainen':
+    if henkilo.sukupuoli != 'nainen':
         # On siis mies tai muu
         if i % 2 == 1 and j % 2 == 1 and poyta[i][j] == null:
-            # On siis parillinen ja parillinen
-            lajittelemattomat[kohta] = poyta[i][j]
+            # On siis pariton ja pariton
+            henkilo = poyta[i][j]
             j += 1
         elif i % 2 == 1 and j % 2 == 0 and poyta[i][j] == null:  # on siis parillinen ja pariton
-            lajittelemattomat[kohta] = poyta[i][j]
+            henkilo = poyta[i][j]
             i += 1
         elif poyta[i][j] != null:
             if j == 2:
@@ -54,12 +55,20 @@ while kohta < n:
             if i == n:
                 i = 0
             if poyta[i + 1][j] == null:
-                poyta[i + 1][j] = lajittelemattomat[kohta]
+                poyta[i + 1][j] = henkilo
             elif poyta[i][j + 1] == null:
-                poyta[i][j + 1] = lajittelemattomat[kohta]
-        kohta += 1
+                poyta[i][j + 1] = henkilo
 
         if j == 2:
             j = 0
         if i == round(n/2):
             n = 0
+
+    lajittelemattomat.remove(henkilo)
+
+def poytaseurueIstumaan(henkilo):
+    for h in henkilo.poytaseurue:
+        if henkilo.poytaseurue(h) != null:
+            istumaan(h)
+
+#TODO lisää jokin, jolla mennään eteenpäin lista
